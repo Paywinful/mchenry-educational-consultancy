de9553877cdfa56dd1914ce9d60753b659938060
Paywinful <fiifipayin@gmail.com>
2025-08-24 02:06:13 +0000

    complete and ready for test 3

--- src/app/api/admin/students/[user_id]/route.ts
+++ src/app/api/admin/students/[user_id]/route.ts
@@ -1,3 +1,5 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 import { NextResponse } from "next/server";
 import { supabaseRoute } from "@/lib/supabase/server";
 import { supabaseAdmin } from "@/lib/supabase/admin";
